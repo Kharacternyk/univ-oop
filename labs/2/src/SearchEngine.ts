9ecfda54347f934a1586f4f478de23dfe4c103bd
Nazar Vinnichuk <nazar@vinnich.uk>
2021-11-14 14:00:04 +0200

    lab2: implement the SearchEngine class

---
+++ labs/2/src/SearchEngine.ts
@@ -0,0 +1,52 @@
+import {TraversalStrategy} from "./TraversalStrategy";
+import {QueryInterpreter} from "../src/QueryInterpreter";
+import {RegexQueryInterpreter} from "../src/RegexQueryInterpreter";
+import {LiteralQueryInterpreter} from "../src/LiteralQueryInterpreter";
+import {WholeWordQueryInterpreterDecorator} from "../src/WholeWordQueryInterpreterDecorator";
+import {
+    CaseInsensitiveQueryInterpreterDecorator
+} from "../src/CaseInsensitiveQueryInterpreterDecorator";
+
+import fs from "fs";
+
+export type SearchOptions = {
+    regex: boolean,
+    caseSensitive: boolean,
+    wholeWord: boolean,
+};
+
+export class SearchEngine {
+    private readonly xmlPath: string;
+    private readonly traversalStrategy: TraversalStrategy;
+    private readonly interpreter: QueryInterpreter;
+
+    public constructor(
+        xmlPath: string,
+        traversalStrategy: TraversalStrategy,
+        searchOptions: SearchOptions,
+    ) {
+        this.xmlPath = xmlPath;
+        this.traversalStrategy = traversalStrategy;
+
+        if (searchOptions.regex) {
+            this.interpreter = new RegexQueryInterpreter();
+        } else {
+            this.interpreter = new LiteralQueryInterpreter();
+        }
+
+        if (!searchOptions.caseSensitive) {
+            this.interpreter = new CaseInsensitiveQueryInterpreterDecorator(this.interpreter);
+        }
+
+        if (searchOptions.wholeWord) {
+            this.interpreter = new WholeWordQueryInterpreterDecorator(this.interpreter);
+        }
+    }
+
+    public getNodeIds(query: string): Array<string> {
+        const regex = this.interpreter.getRegex(query);
+        const xml = fs.readFileSync(this.xmlPath, {encoding: "utf-8"});
+
+        return this.traversalStrategy.getNodeIds(xml, regex);
+    }
+}
